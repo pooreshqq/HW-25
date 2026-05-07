3bf69dd376881ec31a2faf7357a1239ab06538b8
Aktan <aktandzmbkv@gmail.com>
2026-05-07 16:57:24 +0600

    Changed makePayment method to public abstract in abstract class MoneyReceiver and removed field amount in CoinAcceptor

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,15 @@
 package model;
 
-public class CoinAcceptor {
-    private int amount;
+public class CoinAcceptor extends MoneyReceiver {
+    @Override
+    public void makePayment(Product product) {
+        if(this.amount >= product.getPrice()){
+            this.amount -= product.getPrice();
+            System.out.printf("Вы купили %s%n", product.getName());
+        }
+    }
 
     public CoinAcceptor(int amount) {
-        this.amount = amount;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
         this.amount = amount;
     }
 }
--- src/model/MoneyReceiver.java
+++ src/model/MoneyReceiver.java
@@ -2,7 +2,8 @@ package model;
 
 public abstract class MoneyReceiver {
     protected int amount;
-    abstract void makePayment(Product product);
+
+    public abstract void makePayment(Product product);
 
     public int getAmount() {
         return amount;
